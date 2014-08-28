cf52f0cc014001b16456dd7498536869d73cb98b
Jhonsson Córdova <ionszon1806@gmail.com>
2014-08-28 08:26:49 -0500

    27/8/14 - Análisis de gestión de Usuarios
    Se inspeccionó el código para entender el significado del mismo y se
    implemento campos nuevos pensados para la tesis

--- usuarios_logueados/usuarios/models.py
+++ usuarios_logueados/usuarios/models.py
@@ -6,15 +6,15 @@ from django.db import models
 # Create your models here.
 
 #EDICION DE MODELO USER
-User.add_to_class('direccion', models.FloatField(null=True,blank=True))
-User.add_to_class('telefono', models.PositiveIntegerField(null=True,blank=True))
-User.add_to_class('amigos', models.ManyToManyField('self', symmetrical=True,  blank=True))
+User.add_to_class('usuarioSico', models.CharField(max_length=10, null=False, blank=False))
+User.add_to_class('contraseniaSico', models.CharField(max_length=10, null=False, blank=False))
+#User.add_to_class('amigos', models.ManyToManyField('self', symmetrical=True,  blank=True))
 
 #FORMULARIOS
 class SignUpForm(ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'password', 'email', 'first_name', 'last_name']
+        fields = ['username', 'password', 'email', 'first_name', 'last_name', 'usuarioSico', 'contraseniaSico']
         widgets = {
             'password': forms.PasswordInput(),
         }
