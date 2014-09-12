465a2c4b79049cd621c45c0f12f64be94e6cb651
Jhonsson Córdova <ionszon1806@gmail.com>
2014-09-12 15:07:12 -0500

    12/9/14 - Intento fallido I Ajax
    programando asincrinía ajax con django (practica usuarios)

---
+++ usuarios_logueados/usuarios/ajax.py
@@ -0,0 +1,11 @@
+__author__ = 'Andrea Loaiza'
+
+from dajax.core import Dajax
+from dajaxice.decorators import dajaxice_register
+
+@dajaxice_register
+def multiply(request, a, b):
+    dajax = Dajax()
+    result = int(a) * int(b)
+    dajax.assign('#result','value',str(result))
+    return dajax.json()
--- usuarios_logueados/usuarios_logueados/urls.py
+++ usuarios_logueados/usuarios_logueados/urls.py
@@ -1,18 +1,22 @@
 from django.conf.urls import patterns, include, url
-
+from dajaxice.core import dajaxice_autodiscover, dajaxice_config
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.contrib.auth.views import login, logout
 
 admin.autodiscover()
+dajaxice_autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'usuarios_logueados.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
     url(r'^$', 'usuarios.views.main', name='main'),
     url(r'^signup$', 'usuarios.views.signup', name='signup'),
     url(r'^login$', login, {'template_name': 'login.html', }, name="login"),
     url(r'^home$', 'usuarios.views.home', name='home'),
     url(r'^logout$', logout, {'template_name': 'main.html', }, name="logout"),
 )
+
+urlpatterns += staticfiles_urlpatterns()
